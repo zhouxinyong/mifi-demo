62e248eb2920ebfc02c0151865142b6c9ab5524a
zhouxinyong <zhoudaxia5683@163.com>
2018-07-11 16:11:16 +0800

    change baseUrl

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   runtimeCompiler: true,
   lintOnSave: true,
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/mifi-demo/'
+    : './',
   devServer: {
     port: 8089,
     https: true
